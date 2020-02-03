fad4cbf0b11f075aa93b8f602ad6fba73ca98d4d
Baptiste Langlade <baptouuuu@gmail.com>
2020-02-03 18:55:03 +0100

    add psalm

--- src/functions.php
+++ src/functions.php
@@ -3,10 +3,28 @@ declare(strict_types = 1);
 
 namespace Innmind\Stack;
 
+/**
+ * @template T
+ *
+ * @param callable(T): T $function
+ * @param callable(T): T $functions
+ *
+ * @return callable(T): T
+ */
 function stack(callable $function, callable ...$functions): callable {
     $functions = \array_reverse([$function, ...$functions]);
 
+    /**
+     * @psalm-suppress MissingClosureParamType
+     * @psalm-suppress MissingClosureReturnType
+     */
     return static function($inner = null) use ($functions) {
+        /** @var T $inner */
+
+        /**
+         * @psalm-suppress MissingClosureParamType
+         * @psalm-suppress MissingClosureReturnType
+         */
         return \array_reduce(
             $functions,
             static function($inner, callable $outer) {
@@ -17,8 +35,19 @@ function stack(callable $function, callable ...$functions): callable {
     };
 }
 
+/**
+ * @template A
+ * @template R
+ *
+ * @param callable(...A): R $fn
+ * @param A $first
+ *
+ * @return callable(...A): R
+ */
 function curry(callable $fn, ...$first): callable {
+    /** @psalm-suppress MissingClosureParamType */
     return static function(...$second) use ($fn, $first) {
+        /** @var list<A> $second */
         return $fn(...$first, ...$second);
     };
 }
